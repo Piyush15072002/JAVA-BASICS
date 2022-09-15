f4e65d67ca668ff6c4e6bf10d003fdb1ac5cebb5
PIYUSH PANT <piyushpant15@gmail.com>
2022-09-15 13:19:28 +0530

    JAVA

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -2,10 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // * Print
-        // System.out.println("Hello, World!");
-        // System.out.println("This is my first JAVA program");
+        System.out.println("Hello, World!");
+        System.out.println("This is my first JAVA program");
 
+        // int variable
         int powerLevel = 100000;
         System.out.println(powerLevel);
+
     }
 }
---
+++ Project1/src/dataTypes.java
@@ -0,0 +1,23 @@
+public class dataTypes {
+    public static void main(String[] args) throws Exception {
+
+        // there are two types of data types, Primitive and Reference
+
+        // Primitive data types are used for storing the simple values whereas the
+        // Reference data types are used to store the complex datatype
+
+        // some of the primitive data types are
+        // byte store in 1 bytes [-128, 127]
+        // short store in 2 bytes [-32k, 32k]
+        // int store in 4 bytes [-2B, 2B]
+        // long store in 8 bytes [-2B, 2B]
+
+        // for Decimal values
+        // float store in 4 bytes
+        // double store in 8 bytes
+
+        // char store in 2 bytes
+        // boolean store in 1 bytes
+
+    }
+}
