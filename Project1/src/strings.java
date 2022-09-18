40d9196baabea8d91d4bd9b251be088182070a95
PIYUSH PANT <piyushpant15@gmail.com>
2022-09-18 15:18:51 +0530

    JAVA

--- Project1/src/strings.java
+++ Project1/src/strings.java
@@ -26,6 +26,25 @@ public class strings {
 
         // ! Escape Sequences in JAVA
 
+        // To add a ""
+
+        String idea = "I have an \"Idea\" ! "; // put backslash around your ""
+        System.out.println(idea);
+
+        // but if u really need a \ how can you make it escape?
+        // "path is c\asdf\" --> ERROR, therefore the correct method is
+
+        String path = "Dragonballz\\saiyanSaga\\ep123"; // we need to add another \ to make it escape
+        System.out.println(path);
+
+        // New line
+        idea = "\nOkay \nMy Idea is...\n";
+        System.out.println(idea);
+
+        // tab
+        idea = "oh no \t I forgot it";
+        System.out.println(idea);
+
     }
 
 }
