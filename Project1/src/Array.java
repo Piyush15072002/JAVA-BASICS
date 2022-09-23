711a173da3e87b75e6d7fad6fdbb43fea1bef952
PIYUSH PANT <piyushpant15@gmail.com>
2022-09-23 12:31:54 +0530

    JAVA

--- Project1/src/Array.java
+++ Project1/src/Array.java
@@ -1,90 +1,90 @@
 import java.util.Arrays;
 
 public class Array {
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
 
-        // Arrays are like list which contains the values
+                // Arrays are like list which contains the values
 
-        // To create an Array in JAVA, use the datatype int[]
-        // since the arrays are reference type, so u need to allocate the memory using
-        // 'new' keyword, then specify the size of the array
+                // To create an Array in JAVA, use the datatype int[]
+                // since the arrays are reference type, so u need to allocate the memory using
+                // 'new' keyword, then specify the size of the array
 
-        int[] fightersPower = new int[5];
+                int[] fightersPower = new int[5];
 
-        System.out.println(fightersPower);
+                System.out.println(fightersPower);
 
-        // Adding items in the array
-        fightersPower[0] = 100;
-        fightersPower[1] = 100;
-        fightersPower[2] = 50;
-        fightersPower[3] = 70;
-        fightersPower[4] = 100;
+                // Adding items in the array
+                fightersPower[0] = 100;
+                fightersPower[1] = 100;
+                fightersPower[2] = 50;
+                fightersPower[3] = 70;
+                fightersPower[4] = 100;
 
-        // this will just print the address of the memory
-        System.out.println(fightersPower);
+                // this will just print the address of the memory
+                System.out.println(fightersPower);
 
-        // but how can u see the actual items in the arrays
-        // to do that, there is a class in JAVA that allows us called as "Array" which
-        // comes under the java.utils, this class comes with many methods as well
+                // but how can u see the actual items in the arrays
+                // to do that, there is a class in JAVA that allows us called as "Array" which
+                // comes under the java.utils, this class comes with many methods as well
 
-        System.out.println(Arrays.toString(fightersPower));
+                System.out.println(Arrays.toString(fightersPower));
 
-        // ! Another way to make an array is
+                // ! Another way to make an array is
 
-        int[] age = { 21, 22, 22, 25, 25 };
+                int[] age = { 21, 22, 22, 25, 25 };
 
-        // to know its length
-        System.out.println(age.length);
+                // to know its length
+                System.out.println(age.length);
 
-        // To sort
+                // To sort
 
-        Arrays.sort(age);
+                Arrays.sort(age);
 
-        System.out.println(Arrays.toString(age));
+                System.out.println(Arrays.toString(age));
 
-        // ! Multidimensional Arrays
+                // ! Multidimensional Arrays
 
-        // 2 D array (Matrix)
+                // 2 D array (Matrix)
 
-        int[][] area = new int[3][3]; // 3x3 matrix
+                int[][] area = new int[3][3]; // 3x3 matrix
 
-        // if you print this 2D array, u will just get the address and not the actual
-        // data
-        System.out.println(Arrays.toString(area));
+                // if you print this 2D array, u will just get the address and not the actual
+                // data
+                System.out.println(Arrays.toString(area));
 
-        // using this, u can print the 2d array
-        System.out.println(Arrays.deepToString(area));
+                // using this, u can print the 2d array
+                System.out.println(Arrays.deepToString(area));
 
-        // to add the values
-        area[0][0] = 100;
-        area[1][1] = 100;
-        area[2][2] = 100;
+                // to add the values
+                area[0][0] = 100;
+                area[1][1] = 100;
+                area[2][2] = 100;
 
-        System.out.println(Arrays.deepToString(area));
+                System.out.println(Arrays.deepToString(area));
 
-        // Another way to make the array
+                // Another way to make the array
 
-        int[][] subMarks = {
-                {
-                        100, 70
-                },
-                {
-                        90, 80, 50, 60
-                }
-        };
+                int[][] subMarks = {
+                                {
+                                                100, 70
+                                },
+                                {
+                                                90, 80, 50, 60
+                                }
+                };
 
-        System.out.println(Arrays.deepToString(subMarks));
+                System.out.println(Arrays.deepToString(subMarks));
 
-        // 3d array
+                // 3d array
 
-        int[][][] house = new int[5][5][5];
+                int[][][] house = new int[5][5][5];
 
-        house[0][0][0] = 100;
-        house[1][1][1] = 100;
-        house[2][2][2] = 100;
+                house[0][0][0] = 100;
+                house[1][1][1] = 100;
+                house[2][2][2] = 100;
 
-        // to print
-        System.out.println(Array.deepToString(house));
+                // to print
+                System.out.println(Arrays.deepToString(house));
 
-    }
+        }
 }
---
+++ Project1/src/constants.java
@@ -0,0 +1,13 @@
+public class constants {
+    public static void main(String[] args) throws Exception {
+        // just like in the javascript, const and let
+        // where the let variable value can change but the value of the Constant
+        // variable cannot change after it is declared
+        // same way, using "FINAL" keyword, we can make our variable a constant
+
+        final float pi = 3.14F;
+
+        // pi = 100; now we cannot change this
+
+    }
+}
