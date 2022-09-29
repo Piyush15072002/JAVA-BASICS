18ce5b93816dfb16ef9b01d25dc093ee44c1e88c
PIYUSH PANT <piyushpant15@gmail.com>
2022-09-29 13:30:51 +0530

    JAVA

---
+++ Project1/src/math.java
@@ -0,0 +1,11 @@
+public class math {
+    public static void main(String[] args) throws Exception {
+
+        // In java we already have the MATH class and we don't need to import it
+
+        int roundUp = Math.round(2002F);
+
+        System.out.println(roundUp);
+
+    }
+}
