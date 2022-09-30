89014b20dd3d9e1e80467d4342b3bfe6f53a2f25
PIYUSH PANT <piyushpant15@gmail.com>
2022-09-30 14:10:21 +0530

    JAVA

---
+++ Project1/src/operators.java
@@ -0,0 +1,13 @@
+public class operators {
+    public static void main(String[] args) throws Exception {
+        int temp = 40;
+
+        boolean isHot = temp < 50 && temp > 30;
+
+        System.out.println(isHot);
+
+        boolean isNormal = temp == 24 || temp == 25;
+
+        System.out.println(isNormal);
+    }
+}
