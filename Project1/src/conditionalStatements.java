a0bbbd9dd5d3e7c25fcf0f9a2d1a2ee0877b7507
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 11:59:28 +0530

    JAVA

--- Project1/src/conditionalStatements.java
+++ Project1/src/conditionalStatements.java
@@ -17,5 +17,24 @@ public class conditionalStatements {
             System.out.println("You are too old to drive");
         }
 
+        // Switch statement
+
+        String rank = "Captain";
+
+        switch (rank) {
+            // note that, you need to break the loop cuz it will keep executing
+            case "Lieutnant":
+                System.out.println("1st");
+                break;
+
+            case "Captain":
+                System.out.println("2nd");
+                break;
+
+            case "Major":
+                System.out.println("3rd");
+                break;
+
+        }
     }
 }
