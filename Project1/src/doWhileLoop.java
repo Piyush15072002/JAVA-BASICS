d1cffa8fd0382cf916884f94da40ceeb05ffe369
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 13:13:37 +0530

    JAVA

---
+++ Project1/src/doWhileLoop.java
@@ -0,0 +1,15 @@
+public class doWhileLoop {
+    public static void main(String[] args) throws Exception {
+
+        int i = 0;
+
+        do {
+
+            System.out.println(i);
+
+            i--;
+
+        } while (i > 0);
+
+    }
+}
