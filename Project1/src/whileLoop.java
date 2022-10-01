29e1185889b416575aed565e654ce2f7c726efcd
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 13:06:55 +0530

    JAVA

---
+++ Project1/src/whileLoop.java
@@ -0,0 +1,19 @@
+public class whileLoop {
+    public static void main(String[] args) {
+
+        // first we need to initialize a value
+
+        int i = 100;
+
+        while (i >= 0) {
+
+            System.out.print(i + "-");
+
+            // decrementing
+
+            i--;
+
+        }
+
+    }
+}
