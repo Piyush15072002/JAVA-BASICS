0882cf96154908364024978354b7c77d27cbe840
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 18:00:20 +0530

    JAVA

---
+++ Project2/src/OOP1.java
@@ -0,0 +1,47 @@
+// In java, we can create class and their objects
+
+// Creating a class is like defining a blueprint of something
+// Imagine you are making a fighter system where all the best fighters of dragonball universe fighters will be registered
+// before u list all the fighters, u need to create a blueprint of the data u want (like schema), this is called as Class
+
+// Creating a class
+
+class Fighter {
+
+    // The class is defined
+    // Every class have objects, which gets created on the basis of the class
+    // the class have various properties and methods
+    // methods are functions of the class
+
+    // In Fighter class, there can be various properties and methods like
+
+    // * Properties
+
+    String name;
+    int speed;
+    int powerLevel;
+
+    // * Methods
+
+    public static void fight() {
+        System.out.println("Fighting...");
+    }
+
+    public static void attack() {
+        System.out.println("Attack...");
+    }
+
+    public static void defend() {
+        System.out.println("Defend...");
+    }
+
+}
+
+// After you create a class, head to the MAIN() of the main class and create
+// your object there
+
+public class OOP1 {
+    public static void main(String[] args) {
+
+    }
+}
