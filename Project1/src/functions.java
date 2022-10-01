7dd4625a63b591295233a50b277756a0de4c68a4
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 13:56:13 +0530

    JAVA

---
+++ Project1/src/functions.java
@@ -0,0 +1,22 @@
+public class functions {
+
+    // the function is java
+
+    public static void greetMe() {
+        System.out.println("Good Morning!");
+    }
+
+    public static void myName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // calling the functions
+
+        greetMe();
+        myName("Piyush Pant");
+
+    }
+}
