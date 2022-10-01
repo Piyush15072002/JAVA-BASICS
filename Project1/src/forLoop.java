6d65718628a2de3af3364760958c8d2e6da8a71b
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-01 12:57:35 +0530

    JAVA

---
+++ Project1/src/forLoop.java
@@ -0,0 +1,9 @@
+public class forLoop {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 11; i++) {
+            System.out.print(i);
+        }
+
+    }
+}
