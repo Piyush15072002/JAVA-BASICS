ea80cda0504f111b48710386f21ec152157ccd97
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-05 22:42:43 +0530

    MLR

---
+++ Project2/src/Polymorphism.java
@@ -0,0 +1,11 @@
+// Polymorphism means Having many forms
+
+// Polymorphism is of two types
+// 1. Function Overloading
+// 2. Function Overriding
+
+public class Polymorphism {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
