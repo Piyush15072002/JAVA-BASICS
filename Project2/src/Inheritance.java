c06507068b8971046005bc8cd9c5f54c9342c161
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-06 13:23:21 +0530

    MLR

---
+++ Project2/src/Inheritance.java
@@ -0,0 +1,27 @@
+class School {
+
+    String name;
+    int fees;
+
+    public void start() {
+        System.out.println("The school starts now...");
+    }
+
+    public void end() {
+        System.out.println("The school ends now...");
+    }
+
+    // Constructor
+
+    School(String name, int fees) {
+        this.name = name;
+        this.fees = fees;
+    }
+
+}
+
+public class Inheritance {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
