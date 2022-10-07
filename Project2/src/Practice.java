20f0e8c82696e22ac1608378deb4fb18c8bb9fa3
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-07 22:29:12 +0530

    JAVA

--- Project2/src/Practice.java
+++ Project2/src/Practice.java
@@ -1,3 +1,18 @@
+
+class MachineLearning {
+
+    String dataType; // labeled or unlabeled
+
+    public void algorithm() {
+        System.out.println("Algorithm...");
+    }
+
+}
+
+class SupervisedML extends MachineLearning {
+
+}
+
 public class Practice {
     public static void main(String[] args) throws Exception {
 
