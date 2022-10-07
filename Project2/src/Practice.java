d7ee0de0f5743b17faa879d337ff16bf5a615d92
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-07 22:48:46 +0530

    JAVA

--- Project2/src/Practice.java
+++ Project2/src/Practice.java
@@ -3,18 +3,36 @@ class MachineLearning {
 
     String dataType; // labeled or unlabeled
 
-    public void algorithm() {
-        System.out.println("Algorithm...");
+    public void modelSelection(String dataType) {
+        if ((dataType == "Labeled") || (dataType == "labeled")) {
+            System.out.println("Use Supervised Learning");
+
+        } else if ((dataType == "Unlabeled") || (dataType == "unlabeled")) {
+            System.out.println("Use Unsupervised Learning");
+
+        } else {
+            System.out.println("Wrong Value Type");
+        }
     }
 
 }
 
-class SupervisedML extends MachineLearning {
+// class SupervisedLearning extends MachineLearning {
 
-}
+// }
+
+// class UnsupervisedLearning extends MachineLearning {
+
+// }
 
 public class Practice {
     public static void main(String[] args) throws Exception {
+
+        MachineLearning data1 = new MachineLearning();
+
+        data1.dataType = "Labeled";
+
+        data1.modelSelection(data1.dataType);
 
     }
 }
