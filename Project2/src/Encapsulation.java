a9841c74e7ddf624d67fd9223d406ed0b5487d92
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-08 22:28:37 +0530

    JAVA

---
+++ Project2/src/Encapsulation.java
@@ -0,0 +1,8 @@
+// Packages in JAVA - They are like container which carry similiar functions and properties
+// there are two types of packages in java, BUILT-IN and USER-DEFINED
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Project2/src/myPackages.java
@@ -0,0 +1,17 @@
+// Here we will define our own packages and import them to use
+
+public class myPackages {
+    public static void main(String[] args) {
+
+        String[] fighters = new {"Son Goku", "Prince Vegeta", "Son Gohan", "Lord Frieza", "Majin Buu", "Legendary Broly"};
+
+        public void chooseFigther(){
+            int num = Math.random();
+            System.out.println(num);
+            
+        }
+
+        chooseFigther();
+
+    }
+}
