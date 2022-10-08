8716d18639e6ea38c3b14dc801f737085a8bc3ea
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-08 22:40:04 +0530

    JAVA

--- Project2/src/myPackages.java
+++ Project2/src/myPackages.java
@@ -1,17 +1,28 @@
 // Here we will define our own packages and import them to use
 
+// random class
+import java.lang.Math;
+
+class Fighters {
+
+    String[] fighters = { "Son Goku", "Prince Vegeta", "Son Gohan", "Lord Frieza", "Majin Buu", "Legendary Broly" };
+
+    public void chooseFigther() {
+
+        // remember that the random gives a double value so type cast it to the INT
+        double num = Math.random();
+        System.out.println(num);
+
+    }
+
+}
+
 public class myPackages {
     public static void main(String[] args) {
 
-        String[] fighters = new {"Son Goku", "Prince Vegeta", "Son Gohan", "Lord Frieza", "Majin Buu", "Legendary Broly"};
+        Fighters myFighter = new Fighters();
 
-        public void chooseFigther(){
-            int num = Math.random();
-            System.out.println(num);
-            
-        }
-
-        chooseFigther();
+        myFighter.chooseFigther();
 
     }
 }
