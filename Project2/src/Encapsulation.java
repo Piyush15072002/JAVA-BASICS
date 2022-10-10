6ab82a0d7a1f1e1a2d77f6b6de42af8eb2c7e16b
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-10 22:04:26 +0530

    JAVA

--- Project2/src/Encapsulation.java
+++ Project2/src/Encapsulation.java
@@ -1,15 +1,12 @@
 // Packages in JAVA - They are like container which carry similiar functions and properties
 // there are two types of packages in java, BUILT-IN and USER-DEFINED
 
-import Test.*;
-
 // ACCESS MODIFIER
+// They define which thing will be accessible to whom
+// there are 4 types of access modifiers in JAVA, however, there are only 3 in C++
 
 public class Encapsulation {
     public static void main(String[] args) {
-
-        Fighters f1 = new Fighters();
-        f1.chooseFigther();
 
     }
 }
