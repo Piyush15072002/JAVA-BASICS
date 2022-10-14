d365d67dbdd183972a906ea7087a3a53fe686574
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-14 13:47:41 +0530

    JAVA

--- Project2/src/MultipleInheritance.java
+++ Project2/src/MultipleInheritance.java
@@ -28,6 +28,30 @@ class Organization implements Person, Department {
 
 }
 
+interface Employee extends Person, Department {
+
+    int empId = 0;
+
+    void work();
+
+}
+
+class Office implements Employee {
+
+    public void work() {
+        System.out.println("Working...");
+    }
+
+    public void rank() {
+        System.out.println("Authorized");
+    }
+
+    public void name() {
+        System.out.println("Welcome sir");
+    }
+
+}
+
 public class MultipleInheritance {
 
     public static void main(String[] args) throws Exception {
@@ -39,6 +63,12 @@ public class MultipleInheritance {
 
         System.out.println(a.id);
         System.out.println(a.depId);
+
+        Office o = new Office();
+
+        o.name();
+        o.rank();
+        o.work();
 
     }
 
