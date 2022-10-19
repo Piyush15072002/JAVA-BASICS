ba20606548f9d707e28b1dcbc7b2f2cf253b0496
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-19 06:19:33 +0530

    JAVA

--- project3/MySQLConnection.sql
+++ project3/MySQLConnection.sql
@@ -1,5 +1,11 @@
 use javacon;
 
 create table test2(
-	name varchar(50), age (100)
+	name varchar(50), age int(100)
 );
+
+insert into test2(name, age) values("Son Goku", 30);
+insert into test2(name, age) values("Vegeta", 30);
+insert into test2(name, age) values("Son Gohan", 30);
+
+select * from test2;
--- project3/src/App.java
+++ project3/src/App.java
@@ -6,5 +6,31 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://localhost:3306/javacon";
+        String username = "piyush";
+        String password = "pant";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        Statement statement = connection.createStatement();
+
+        // String query = "insert into test2(name, age) values('Trunks',20)";
+        String query = "select * from test2 where age=30";
+
+        ResultSet RS = statement.executeQuery(query);
+
+        while (RS.next()) {
+
+            int age = RS.getInt("age");
+            String name = RS.getString("name");
+            System.out.println(name + " - " + age);
+
+        }
+
+        RS.close();
+        statement.close();
+        connection.close();
     }
 }
