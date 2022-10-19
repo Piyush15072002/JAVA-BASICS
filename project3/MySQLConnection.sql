d18dc154baa6ea4ad28e2f78985f3405fc4150fb
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-19 05:58:30 +0530

    JAVA

---
+++ project3/MySQLConnection.sql
@@ -0,0 +1,5 @@
+use javacon;
+
+create table test2(
+	name varchar(50), age (100)
+);
--- project3/src/App.java
+++ project3/src/App.java
@@ -1,5 +1,10 @@
+import java.sql.*;
+
 public class App {
+
+    // Database connectivity
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
     }
 }
