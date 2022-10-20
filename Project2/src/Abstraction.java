b1b1df3b530bc34d6b3d0b046c8ee3682afb5c79
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-20 14:05:45 +0530

    JAVA

---
+++ Project2/src/Abstraction.java
@@ -0,0 +1,18 @@
+// To perform abstraction, we make abstraction class which must have at least one abstraction functions
+// using the abstraction keyword
+
+abstract class Bank {
+
+    // these functions and properties are like Blueprints so we don't define them
+    // and just delcare them
+    String id;
+
+    abstract public void withdrawMoney();
+
+}
+
+public class Abstraction {
+    public static void main(String[] args) {
+
+    }
+}
