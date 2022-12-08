ec537668f170b4b7341e99af3e3dc3995045a89a
PIYUSH PANT <piyushpant15@gmail.com>
2022-12-08 21:50:44 +0530

    Practical

--- JavaPracticeForPracticals/src/practical1.java
+++ JavaPracticeForPracticals/src/practical1.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class practical1 {
     public static void main(String args[]) {
 
+        // program to calculate the percent, grades of student
+
+        Scanner marks = new Scanner(System.in);
+        System.out.println("Enter your marks : ");
+
+        float a = marks.nextInt();
+        float b = marks.nextInt();
+        float c = marks.nextInt();
+        float d = marks.nextInt();
+        float e = marks.nextInt();
+
+        float total = a + b + c + d + e;
+
+        float percentage = (float) (total / 500) * 100;
+
+        System.out.println("Your percentage is : " + percentage);
+
+        if (percentage >= 90) {
+            System.out.println("A");
+        } else if (percentage < 90 && percentage >= 80) {
+            System.out.println("B");
+        } else if (percentage < 80 && percentage >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("Fail");
+        }
     }
+
 }
---
+++ JavaPracticeForPracticals/src/practical2.java
@@ -0,0 +1,10 @@
+public class practical2 {
+    public static void main(String[] args) throws Exception {
+
+        int age = 100;
+        if (age > 100) {
+
+        }
+
+    }
+}
