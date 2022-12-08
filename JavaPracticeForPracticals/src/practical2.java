0b9312e9e91888e07ea4672b3e47d6b4eb1a7d17
PIYUSH PANT <piyushpant15@gmail.com>
2022-12-08 22:22:00 +0530

    Practical

--- JavaPracticeForPracticals/src/practical2.java
+++ JavaPracticeForPracticals/src/practical2.java
@@ -3,7 +3,30 @@ public class practical2 {
 
         int age = 100;
         if (age > 100) {
+            System.out.println("Damn you are old");
+        } else {
+            System.out.println("Live your life fully");
+        }
 
+        switch (age) {
+            case 50:
+                System.out.println("Half century");
+                break;
+            case 100:
+                System.out.println("Old");
+                break;
+            default:
+                System.out.println("No age");
+
+        }
+
+        for (int i = 0; i <= 100; i = i + 10) {
+            System.out.println(i);
+        }
+
+        while (age < 201) {
+            System.out.println(age);
+            age = age + 10;
         }
 
     }
